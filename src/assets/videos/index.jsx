4ff03b2db18f927ddee9cbaabe41c87e30a0a9ba
Rameshadhikari1999 <rameshadhikari199917@gmail.com>
2024-08-18 16:10:00 +0530

    bug: fix video path

--- src/assets/videos/index.jsx
+++ src/assets/videos/index.jsx
@@ -2,6 +2,6 @@ import video1 from './pVideo1.mp4';
 import video2 from './pVideo2.mp4';
 import video3 from './video1.mp4';
 import video4 from './pVideo3.mp4';
-import video5 from './pVideo4.mp4';
+import video5 from './pvideo4.mp4';
 
 export { video1, video2, video3, video4, video5 }
